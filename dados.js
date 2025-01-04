4646276bd23e1b2cf61ef2a3bd6a2bbe300eb63d
Lucas Machado Araujo <lucas.machado@einstein.br>
2025-01-04 10:58:31 -0300

    add o Dragon no banco de dados

--- dados.js
+++ dados.js
@@ -2,22 +2,22 @@ let dados = [
     {
         titulo: 'Dragon',
         descricao: 'A palavra "dragon" tem raízes indo-européias e é a desiginação mais comum para essas criaturas em muitas culturas ocidentais. Geralmente descritos como grandes répteis alados, com escamas, garras afiadas e a capacidade de cuspir fogo. Sua aparência e poderes variam muito dependendo da mitologia. Representam poder, força, sabedoria e, em algumas culturas, até mesmo a criação.', 
-        tags: 'fogo dragão dragões'
+        tags: 'fogo dragão dragões dragons'
     },
     {
         titulo: 'Drake',
         descricao: 'O drake é frequentemente descrito como um dragão menor ou mais jovem, com características semelhantes aos dragões, mas geralmente com menos poder e habilidades. Em algumas mitologias, drakes são considerados estágios intermediários na evolução de um dragão completo.',
-        tags: 'bebe dragão menor jovem'
+        tags: 'bebe dragão menor jovem dragons'
     },
     {
         titulo: 'Wyvern',
         descricao: 'O wyvern é uma criatura semelhante a um dragão, mas com apenas duas patas e uma cauda que termina em um ferrão venenoso. Eles são frequentemente retratados como mais ágeis e rápidos que os dragões, e são conhecidos por sua natureza agressiva.',
-        tags: 'galinha dragões ferrão drogon'
+        tags: 'galinha dragões ferrão drogon dragons'
     },
     {
         titulo: 'Fae',
         descricao: 'Dragões Fae são criaturas míticas que surgem da fusão entre a mitologia dos dragões e a do povo Fae, ou seja, as fadas. Essa combinação resulta em seres fascinantes, com características únicas que os diferenciam tanto dos dragões tradicionais quanto das fadas clássicas.A aparência dos dragões Fae pode variar muito, dependendo da tradição e da história. No entanto, algumas características comuns',
-        tags: 'dragãozinho dragões'
+        tags: 'dragãozinho dragões dragons'
     }
     
 ];
